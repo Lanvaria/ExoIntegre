3799dc3d32d6ffbb05410eb1493fc0580283adf0
Lanvaria <Alexandrafcspro@gmail.com>
2025-03-13 09:47:26 +0100

    9h47 13/03

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-  const nav = document.querySelector('.nav')
-  const burger = document.querySelector('.burger')
-  nav.classList.toggle('open')
-  burger.classList.toggle('open')
+  const nav = document.querySelector('.nav');
+  const burger = document.querySelector('.burger');
+  nav.classList.toggle('open');
+  burger.classList.toggle('open');
 }
